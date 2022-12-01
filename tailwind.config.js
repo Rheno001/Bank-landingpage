8c925561ae6a7a2b866b1995bc67a3d426b51105
Rheno001 <renfredreuben@gmail.com>
2022-12-01 17:14:05 +0100

    completed

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'darkBlue': 'hsl(233, 26%, 24%)',
+        'limeGreen':'hsl(136, 65%, 51%)',
+        'brightCyan': 'hsl(192, 70%, 51%)',
+        'greyishBlue': 'hsl(233, 8%, 62%)',
+        'lightgreyishBlue': 'hsl(220, 16%, 96%)',
+        'white': 'hsl(0, 0%, 100%)'
+      }
+    },
+  },
+  plugins: [],
+}
