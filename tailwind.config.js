af9f5fce110ec77decfd4709a75272d16f179545
Rheno001 <renfredreuben@gmail.com>
2022-12-02 10:52:32 +0100

    hover states

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily:{
+
+      },
       colors:{
         'darkBlue': 'hsl(233, 26%, 24%)',
         'limeGreen':'hsl(136, 65%, 51%)',
         'brightCyan': 'hsl(192, 70%, 51%)',
         'greyishBlue': 'hsl(233, 8%, 62%)',
         'lightgreyishBlue': 'hsl(220, 16%, 96%)',
-        'white': 'hsl(0, 0%, 100%)'
+        'white': 'hsl(0, 0%, 100%)',
       }
     },
   },
